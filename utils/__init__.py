47189c0118b51160d9813668c0674035055592ee
Met091 <145667573+Met091@users.noreply.github.com>
2025-05-09 12:50:55 +0800

    Create __init__.py

---
+++ utils/__init__.py
@@ -0,0 +1,7 @@
+# utils/__init__.py
+# This file makes 'utils' a Python package.
+# It contains helper functions and utility modules used across the application.
+
+# from .file_utils import read_file, save_file
+# from .logger import app_logger
+# from .session_manager import initialize_session_state
