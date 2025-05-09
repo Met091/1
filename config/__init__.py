f4a354a62db2187fea9ab2f084f1b030194ebe4b
Met091 <145667573+Met091@users.noreply.github.com>
2025-05-09 12:49:42 +0800

    Create __init__.py

---
+++ config/__init__.py
@@ -0,0 +1,5 @@
+# config/__init__.py
+# This file makes 'config' a Python package.
+# It allows for easy importing of settings or other configurations.
+
+# from .settings import GOOGLE_API_KEY, WORKSPACE_DIR # etc.
